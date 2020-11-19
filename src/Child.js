403602a43cddb143cdc83c185fcddc6cf565c49c
mubashar <zeeshan64kb@gmail.com>
2020-11-19 08:54:05 +0500

    áttach repo

---
+++ src/Child.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Child from './Child';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Child/>
+
+    </div>
+  );
+}
+
+export default App;
